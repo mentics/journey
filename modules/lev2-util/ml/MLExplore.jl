061fc7c07ede4e720a9032adbdc3b6563b19d1dd
Joel Shellman <joel@mentics.com>
2023-04-23 07:02:33 -0700

    sync

--- modules/lev2-util/ml/MLExplore.jl
+++ modules/lev2-util/ml/MLExplore.jl
@@ -2,6 +2,8 @@ module MLExplore
 using Flux, Transformers, CUDA
 using Compat
 
+# Fluxml expects batch to be the last dimension
+
 # const DATA_SPLIT4 = NamedTuple{(:train, :validation), Tuple{A,B}} where {A<:AbstractArray,B<:AbstractArray}
 # const DATA_XY_SPLIT = NamedTuple{(:train, :validation), Tuple{A,B}} where {A<:AbstractArray,B<:AbstractArray}
 
@@ -20,15 +22,7 @@ mlx.run()
 
 # This outputs a lenth x 2 matrix (sequence)
 function makeOrigSeqSinCos(length = 1000)
-    res = reduce(vcat, [sin(i/100.0) cos(i/100.0)] for i in 1:length)
-
-    # res = Array{Float64}(undef, 0, 2)
-    # for i in 1:length
-    #     x1 = sin(i/100.0)
-    #     x2 = cos(i/100.0)
-    #     vcat(res, [x1 x2])
-    # end
-
+    res = reduce(vcat, [Float32(sin(i/100.0)) Float32(cos(i/100.0))] for i in 1:length)
     return res
 end
 
@@ -48,8 +42,10 @@ function run(params=makeParams(), input=makeOrigSeqSinCos())
     dataTokens = tokenizeData(input)
     dataSplit = splitTrainValidation(dataTokens)
     xySplitBatched = map(d -> makeBatches(params.model.block_size, d), dataSplit)
-    model = makeModel1(params.model)
+    global model = makeModel1(params.model)
     train!(params, model, xySplitBatched)
+    start = rand(Float32, params.model.block_size, 2, 1)
+    generate(model, start, 10);
 end
 
 # This doesn't have to return the same size as the input
@@ -70,23 +66,11 @@ end
 function makeBatches(block_size, dataTokens)
     ts_count = size(dataTokens, 1)
     num = ts_count - block_size - 1
-    # x = [dataTokens[i:i + block_size - 1] for i in 1:num]
-    # y = [dataTokens[i + 1:i + block_size] for i in 1:num]
-
     @assert size(dataTokens) == (ts_count,2) "$(size(dataTokens)) == $((num,2))"
-    x = stack(dataTokens[i:i + block_size - 1,:] for i in 1:num; dims=1)
-    y = stack(dataTokens[i:i + block_size - 1,:] for i in 1:num; dims=1)
-    @assert size(x) == (num, block_size, 2) "$(size(x)) == $((num, block_size, 2))"
-
-    # x = Array{Float64}(undef, 0, ndims(dataTokens) + 1)
-    # y = Array{Float64}(undef, 0, ndims(dataTokens) + 1)
-    # for _ in 1:num
-    #     # TODO: use LFSR to get both random, and complete, and no duplication
-    #     i = rand(1:num)
-    #     x = dataTokens[i:i + block_size]
-    #     y = dataTokens[i + 1:i + block_size + 1]
-    # end
-
+    # Batch is last dim and that's the default for stack
+    x = stack(dataTokens[i:i + block_size - 1,:] for i in 1:num)
+    y = stack(dataTokens[i:i + block_size - 1,:] for i in 1:num)
+    @assert size(x) == (block_size, 2, num) "$(size(x)) == $((block_size, 2, num))"
     return (; x, y)
 end
 
@@ -98,15 +82,32 @@ end
 
 function makeModel1(paramsModel)
     return Flux.Dense(paramsModel.block_size => paramsModel.block_size)
+    # return Flux.Dense(paramsModel.block_size => 1)
 end
 
 function train!(params, model, data)
     global dd = data
-    x1 = data.train.x[1,:,:]
-    y1 = data.train.y[1,:,:]
+    x1 = data.train.x[:,:,1]
+    y1 = data.train.y[:,:,1]
     println(typeof(x1), ' ', size(x1))
     yhat1 = model(x1)
     Flux.Losses.logitcrossentropy(yhat1, y1)
+end
+
+function generate(model, start, forecast_count)
+    # T x C x B
+    @assert size(start) == (8, 2, 1) string("expected ", (8, 2, 1), " but was ", size(start))
+    res = start
+    for _=1:forecast_count
+        y = model(res[end-7:end,:,:])
+        println("y size: ", size(y))
+        yend = y[end:end,:,:]
+        println("yend size: ", size(yend))
+        # logitsLast = logits[:,end,:]
+        # probs = Flux.softmax(logitsLast)
+        res = cat(res, yend; dims=1)
+    end
+    return res
 end
 
 end
