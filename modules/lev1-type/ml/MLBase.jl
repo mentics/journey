150730ac5299ad268f4283a656e8d23079320821
Joel Shellman <joel@mentics.com>
2023-05-23 02:44:54 -0700

    sync

--- modules/lev1-type/ml/MLBase.jl
+++ modules/lev1-type/ml/MLBase.jl
@@ -1,7 +1,7 @@
 module MLBase
 using Intervals
 
-const DateRange = Ref(Interval{Closed,Open}(Date(2020,2,1), Date(2020,7,1)))
-const DateRangeExt = Ref(Interval{Closed,Open}(Date(2019,11,1), Date(2020,11,30)))
+const DateRange = Ref(Interval{Closed,Open}(Date(2020,2,1), Date(2020,3,1)))
+const DateRangeExt = Ref(Interval{Closed,Open}(Date(2019,12,1), Date(2020,5,31)))
 
 end
