77f7092d83d5c49e400f7a4988b8946792b72b71
Joel Shellman <joel@mentics.com>
2023-11-21 14:05:36 -0800

    sync

--- headless.sh
+++ headless.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-DISPLAY=:0 xvfb-run -s '-screen 0 1024x768x24' -- julia --threads auto --project --load scripts/repl-${1}.jl
+echo "|$1|"
+if [ -z "${1}" ]; then
+  DISPLAY=:0 xvfb-run -s '-screen 0 1024x768x24' -- julia --threads auto --project
+else
+  DISPLAY=:0 xvfb-run -s '-screen 0 1024x768x24' -- julia --threads auto --project --load scripts/repl-${1}.jl
+fi
